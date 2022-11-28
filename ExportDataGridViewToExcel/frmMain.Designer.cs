cc93438133e67b6479c539b6088f318afd77945e
MrdKongolo <medardbaraka09@gmail.com>
2022-11-28 15:44:28 +0200

    first commits

---
+++ ExportDataGridViewToExcel/frmMain.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace ExportDataGridViewToExcel
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.btnExporter = new System.Windows.Forms.Button();
+            this.lblEtat = new System.Windows.Forms.Label();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.productIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.unitPriceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.unitsInStockDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.progressBar.Location = new System.Drawing.Point(12, 327);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(545, 23);
+            this.progressBar.TabIndex = 0;
+            // 
+            // btnExporter
+            // 
+            this.btnExporter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExporter.Location = new System.Drawing.Point(563, 327);
+            this.btnExporter.Name = "btnExporter";
+            this.btnExporter.Size = new System.Drawing.Size(75, 23);
+            this.btnExporter.TabIndex = 1;
+            this.btnExporter.Text = "&Exporter";
+            this.btnExporter.UseVisualStyleBackColor = true;
+            this.btnExporter.Click += new System.EventHandler(this.btnExporter_Click);
+            // 
+            // lblEtat
+            // 
+            this.lblEtat.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblEtat.AutoSize = true;
+            this.lblEtat.Location = new System.Drawing.Point(284, 353);
+            this.lblEtat.Name = "lblEtat";
+            this.lblEtat.Size = new System.Drawing.Size(80, 13);
+            this.lblEtat.TabIndex = 2;
+            this.lblEtat.Text = "Traitement...0%";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.AutoGenerateColumns = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.productIDDataGridViewTextBoxColumn,
+            this.productNameDataGridViewTextBoxColumn,
+            this.unitPriceDataGridViewTextBoxColumn,
+            this.unitsInStockDataGridViewTextBoxColumn});
+            this.dataGridView.DataSource = this.productBindingSource;
+            this.dataGridView.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(626, 309);
+            this.dataGridView.TabIndex = 3;
+            // 
+            // productIDDataGridViewTextBoxColumn
+            // 
+            this.productIDDataGridViewTextBoxColumn.DataPropertyName = "ProductID";
+            this.productIDDataGridViewTextBoxColumn.HeaderText = "ProductID";
+            this.productIDDataGridViewTextBoxColumn.Name = "productIDDataGridViewTextBoxColumn";
+            // 
+            // productNameDataGridViewTextBoxColumn
+            // 
+            this.productNameDataGridViewTextBoxColumn.DataPropertyName = "ProductName";
+            this.productNameDataGridViewTextBoxColumn.HeaderText = "Product Name";
+            this.productNameDataGridViewTextBoxColumn.Name = "productNameDataGridViewTextBoxColumn";
+            this.productNameDataGridViewTextBoxColumn.Width = 250;
+            // 
+            // unitPriceDataGridViewTextBoxColumn
+            // 
+            this.unitPriceDataGridViewTextBoxColumn.DataPropertyName = "UnitPrice";
+            this.unitPriceDataGridViewTextBoxColumn.HeaderText = "Price";
+            this.unitPriceDataGridViewTextBoxColumn.Name = "unitPriceDataGridViewTextBoxColumn";
+            // 
+            // unitsInStockDataGridViewTextBoxColumn
+            // 
+            this.unitsInStockDataGridViewTextBoxColumn.DataPropertyName = "UnitsInStock";
+            this.unitsInStockDataGridViewTextBoxColumn.HeaderText = "UnitsInStock";
+            this.unitsInStockDataGridViewTextBoxColumn.Name = "unitsInStockDataGridViewTextBoxColumn";
+            // 
+            // productBindingSource
+            // 
+            this.productBindingSource.DataSource = typeof(ExportDataGridViewToExcel.Product);
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.WorkerSupportsCancellation = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 369);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.lblEtat);
+            this.Controls.Add(this.btnExporter);
+            this.Controls.Add(this.progressBar);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Exporting datagridview to excel file";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button btnExporter;
+        private System.Windows.Forms.Label lblEtat;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn unitPriceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn unitsInStockDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource productBindingSource;
+    }
+}
+
---
+++ ExportDataGridViewToExcel/frmMain.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExportDataGridViewToExcel
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            List<Product> liste = ((DataParameter)e.Argument).ProductList;
+            string nomFichier = ((DataParameter)e.Argument).nomFichier;
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+            Workbook wb = excel.Workbooks.Add(XlSheetType.xlWorksheet);
+            Worksheet ws = (excel).ActiveSheet;
+            excel.Visible = false;
+            int index = 1;
+            int process = liste.Count;
+
+            //Add column
+            ws.Cells[1, 1] = "ProductID";
+            ws.Cells[1, 2] = "Product Name";
+            ws.Cells[1, 3] = "Price";
+            ws.Cells[1, 4] = "Stock";
+            foreach (Product p in liste)
+            {
+                if(!backgroundWorker.CancellationPending)
+                {
+                    backgroundWorker.ReportProgress(index++ * 100 / process);
+                    ws.Cells[index, 1] = p.CategoryID.ToString();
+                    ws.Cells[index, 2] = p.ProductName;
+                    ws.Cells[index, 3] = p.UnitPrice.ToString();
+                    ws.Cells[index, 4] = p.UnitsInStock.ToString();
+                }
+            }
+            ws.SaveAs(nomFichier, XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing, true, false, XlSaveAsAccessMode.xlNoChange, XlSaveConflictResolution.xlLocalSessionChanges, Type.Missing, Type.Missing);
+            excel.Quit();
+        }
+        struct DataParameter
+        {
+            public List<Product> ProductList;
+            public string nomFichier { get; set; }
+        }
+        DataParameter _inputParameter;
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            using(NorthwindEntities db  = new NorthwindEntities())
+            {
+                productBindingSource.DataSource = db.Products.ToList();
+            }
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+            lblEtat.Text = string.Format("Traitement...{0}", e.ProgressPercentage);
+            progressBar.Update();
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error==null)
+            {
+                Thread.Sleep(100);
+                lblEtat.Text = "Vos données ont été exportées avec succès !";
+            }
+        }
+
+        private void btnExporter_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker.IsBusy)
+                return;
+            using(SaveFileDialog sfd = new SaveFileDialog() { Filter = "Fichiel Excel |*.xls" })
+            {
+                if(sfd.ShowDialog() == DialogResult.OK)
+                {
+                    _inputParameter.nomFichier = sfd.FileName;
+                    _inputParameter.ProductList = productBindingSource.DataSource as List<Product>;
+                    progressBar.Minimum = 0;
+                    progressBar.Value = 0;
+                    backgroundWorker.RunWorkerAsync(_inputParameter);
+                }
+            }
+        }
+    }
+}
